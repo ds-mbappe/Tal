35905de4c01e70a0a039bc568a816681e215a487
Daniel Stéphane <mbappedaniel00@yahoo.fr>
2023-01-02 13:49:59 +0100

    Final adjustements & clean up

--- .history/AuthProvider_20230102134054.js
+++ .history/AuthProvider_20230102134054.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState } from "react";
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [isLoggedin, setIsLoggedIn] = useState(false);
---
+++ data.js
@@ -0,0 +1,125 @@
+export const data = [
+  {
+    id: 0,
+    image: "https://placeimg.com/480/480/any",
+    name: "Emy Ratajkowski",
+    talcsign: "emrata",
+  },
+  {
+    id: 1,
+    image: "https://placeimg.com/480/480/any",
+    name: "Christian",
+    talcsign: "lilchristd",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/480/480/any",
+    name: "Le Duc",
+    talcsign: "owona42",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/480/480/any",
+    name: "Noe Yvan Benam",
+    talcsign: "almight-bny",
+  },
+  {
+    id: 4,
+    image: "https://placeimg.com/480/480/any",
+    name: "L Y O R D",
+    talcsign: "j.a.l.e.s",
+  },
+  {
+    id: 5,
+    image: "https://placeimg.com/480/480/any",
+    name: "Karim Betchem",
+    talcsign: "karim_betchem",
+  },
+  {
+    id: 6,
+    image: "https://placeimg.com/480/480/any",
+    name: "SIEWE Loic",
+    talcsign: "siewe_loic",
+  },
+  {
+    id: 7,
+    image: "https://placeimg.com/480/480/any",
+    name: "Laetitia Kamge",
+    talcsign: "laety.k",
+  },
+  {
+    id: 8,
+    image: "https://placeimg.com/480/480/any",
+    name: "Mounira",
+    talcsign: "mounira_nk",
+  },
+  {
+    id: 9,
+    image: "https://placeimg.com/480/480/any",
+    name: "Cindy Karen Kuekam",
+    talcsign: "cindy__kuekam",
+  },
+  {
+    id: 10,
+    image: "https://placeimg.com/480/480/any",
+    name: "German Ondoko",
+    talcsign: "ogthevie",
+  },
+  {
+    id: 11,
+    image: "https://placeimg.com/480/480/any",
+    name: "Daniel Mbappe",
+    talcsign: "ds.mbappe",
+  },
+  {
+    id: 12,
+    image: "https://placeimg.com/480/480/any",
+    name: "Marie Destiny",
+    talcsign: "marie.d",
+  },
+  {
+    id: 13,
+    image: "https://placeimg.com/480/480/any",
+    name: "Alexandre Maxime",
+    talcsign: "maxime.al",
+  },
+  {
+    id: 14,
+    image: "https://placeimg.com/480/480/any",
+    name: "Biamou Chelsea",
+    talcsign: "chelsy.stevia",
+  },
+  {
+    id: 15,
+    image: "https://placeimg.com/480/480/any",
+    name: "Le Yoro",
+    talcsign: "uniqueYoro",
+  },
+];
+
+export const postData = [
+  {
+    id: 0,
+    image: "https://placeimg.com/640/480/any",
+    postActualText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque cras non tincidunt fermentum felis, est nunc sit. Nunc at feugiat ipsum ultrices. Ornare massa iaculis in nulla ullamcorper aliquam. Mollis cras adipiscing libero, sagittis cursus mauris, eu turpis mattis.",
+  },
+  {
+    id: 1,
+    image: "",
+    postActualText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque cras non tincidunt fermentum felis, est nunc sit. Nunc at feugiat ipsum ultrices. Ornare massa iaculis in nulla ullamcorper aliquam. Mollis cras adipiscing libero, sagittis cursus mauris, eu turpis mattis.",
+  },
+  {
+    id: 2,
+    image: "",
+    postActualText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque cras non tincidunt fermentum felis, est nunc sit. Nunc at feugiat ipsum ultrices. Ornare massa iaculis in nulla ullamcorper aliquam. Mollis cras adipiscing libero, sagittis cursus mauris, eu turpis mattis.",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/640/480/any",
+    postActualText:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque cras non tincidunt fermentum felis, est nunc sit. Nunc at feugiat ipsum ultrices. Ornare massa iaculis in nulla ullamcorper aliquam. Mollis cras adipiscing libero, sagittis cursus mauris, eu turpis mattis.",
+  },
+];
